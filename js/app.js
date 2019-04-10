2801e686c35cb94fe2039aa451387d7dcf3cf397
Imrager <davidlclarke92@yahoo.com>
2019-04-10 10:59:45 -0400

    added more html and function to grab position of word works

--- js/app.js
+++ js/app.js
@@ -1,13 +1,29 @@
-const hangmanWord = "El-Dorado"
+// const hangmanWord = ["Dorado","Cowbell", "Cowboy", "Indian", "Tobacco"]
+
+
+// function randomWord (){hangmanWord[Math.floor(Math.random() * 4) + 1]}
+//  random number from https://stackoverflow.com/questions/4959975/generate-random-number-between-two-numbers-in-javascript
+document.getElementById('newGame').addEventListener('click', function(){
+    location.reload()
+    console.logg(addLetterDivs())
+
+})
+function addLetterDivs(){
+    return document.getElementsByTagName('article').appendChild(document.createElement('div').appendChild(document.createTextNode('hey')))
+}
+
+let hangmanWord = "Opportunity"
 let letterGuess = "o"
-let correctLetters = "         "
-console.log(correctLetters)
+let correctLetter = []
+console.log(correctLetter)
 function checkLetters (word, letter){
     for(let i = 0; i < word.length; i++){
+        
         if(word[i].toLowerCase() === letter.toLowerCase()){
-            correctLetters.replace(correctLetters[i], word[i])
+            correctLetter[i] =  letterGuess
+            
         }  
     }
 }
 checkLetters(hangmanWord, letterGuess)
-console.log(correctLetters)
+console.log(correctLetter)
