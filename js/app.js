278159678d36374ac23d55703bc8d8c51f6286fe
Imrager <davidlclarke92@yahoo.com>
2019-04-10 13:05:32 -0400

    added jquery added letter placement on html

--- js/app.js
+++ js/app.js
@@ -3,27 +3,26 @@
 
 // function randomWord (){hangmanWord[Math.floor(Math.random() * 4) + 1]}
 //  random number from https://stackoverflow.com/questions/4959975/generate-random-number-between-two-numbers-in-javascript
-document.getElementById('newGame').addEventListener('click', function(){
-    location.reload()
-    console.logg(addLetterDivs())
+$(document).ready(function () {
+    let hangmanWord = "Opportunity"
+    $('#newGame').click(function () {
+        for(i=0; i < hangmanWord.length; i++){
+            $('<div class="letterDiv"></div>').text(i).appendTo('article')
+        }
+    })
 
-})
-function addLetterDivs(){
-    return document.getElementsByTagName('article').appendChild(document.createElement('div').appendChild(document.createTextNode('hey')))
-}
+    let letterGuess = "o"
+    let correctLetter = []
+    console.log(correctLetter)
+    function checkLetters(word, letter) {
+        for (let i = 0; i < word.length; i++) {
 
-let hangmanWord = "Opportunity"
-let letterGuess = "o"
-let correctLetter = []
-console.log(correctLetter)
-function checkLetters (word, letter){
-    for(let i = 0; i < word.length; i++){
-        
-        if(word[i].toLowerCase() === letter.toLowerCase()){
-            correctLetter[i] =  letterGuess
-            
-        }  
+            if (word[i].toLowerCase() === letter.toLowerCase()) {
+                correctLetter[i] = letterGuess
+
+            }
+        }
     }
-}
-checkLetters(hangmanWord, letterGuess)
-console.log(correctLetter)
+    checkLetters(hangmanWord, letterGuess)
+    console.log(correctLetter)
+})
