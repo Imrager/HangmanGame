4b07c49b079320881a68f008b6d57a6e854c121f
Imrager <davidlclarke92@yahoo.com>
2019-04-11 12:26:22 -0400

    gonna make three game modes first game mode 3 letter words

--- js/app.js
+++ js/app.js
@@ -3,11 +3,11 @@
 
 // function randomWord (){hangmanWord[Math.floor(Math.random() * 4) + 1]}
 //  random number from https://stackoverflow.com/questions/4959975/generate-random-number-between-two-numbers-in-javascript
-let hangmanWord = "Yes"
+let hangmanWord = "poo"
 let letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 let letterGuess = $('input').val()
 let correctLetter = []
-let hangmanImages = ['images/Hangman Pic/hangmanHead.png', 'images/Hangman Pic/hangmanBody.png','images/Hangman Pic/hangmanNorightArm .png','images/Hangman Pic/hangmanNoLegs.png','images/Hangman Pic/hangmanNoRightLeg.png']
+let hangmanImages = ['images/Hangman Pic/hangman base.png','images/Hangman Pic/hangmanHead.png', 'images/Hangman Pic/hangmanBody.png','images/Hangman Pic/hangmanNorightArm .png','images/Hangman Pic/hangmanNoLegs.png','images/Hangman Pic/hangmanNoRightLeg.png', 'images/Hangman Pic/hangmanLose.png']
 $(document).ready(function () {
     console.log($('input').val())
     // When click new game adds empty letter placement 
@@ -37,11 +37,30 @@ $(document).ready(function () {
             if (hangmanWord[i].toLowerCase() === letterGuess.toLowerCase()) {
                 $('.letterDiv')[i].innerText = letterGuess
             }
-            // changing image source https://stackoverflow.com/questions/554273/changing-the-image-source-using-jquery
-            else{
-                $('img').attr('src', 'images/Hangman Pic/hangmanHead.png' )
+        }
+        for(let i =0; i < letters.length; i++){
+            if($('.alphabet')[i] === letterGuess){
                 
             }
+        }
+        // changing image source https://stackoverflow.com/questions/554273/changing-the-image-source-using-jquery
+        if(hangmanWord.length = 3){if ($('.letterDiv')[0].innerText === letterGuess ||
+        $('.letterDiv')[1].innerText === letterGuess ||
+        $('.letterDiv')[2].innerText === letterGuess 
+        ){
+            $('img').attr('src', hangmanImages[0]) 
+        }
+        else{
+            hangmanImages.shift() && $('img').attr('src', hangmanImages[0])
+        }}
+        
+        if(($('.letterDiv')[0].innerText === hangmanWord[0].toLowerCase()) && 
+        ($('.letterDiv')[1].innerText === hangmanWord[1]) && 
+        ($('.letterDiv')[2].innerText === hangmanWord[2])){
+            alert('you won!')
+        }
+        if($('img').attr('src') === hangmanWord[hangmanWord.length - 1]){
+            alert('you lose!')
         }
     })
 })
